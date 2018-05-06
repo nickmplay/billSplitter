896175e3e74dda267aa140f38b621d0a556e62f4
Nick M <nickmontagni@hotmail.com>
2018-05-06 21:10:42 +0100

    finished Bill CRUD

--- src/bill.js
+++ src/bill.js
@@ -1,6 +1,6 @@
 //Person object to be instantiated into an array of people
-function Person (id){
-  return {id:id, type:"split"}
+function Person (){
+  return {id: Date.now(), type:"split", amount:0}
 };
 
 //Bill object that contains bill methods and properties
@@ -8,18 +8,53 @@ function Bill (amount, service = true) {
   this.amount = amount;
   this.service = service;
   this.people = [
-    new Person(1),
-    new Person(2)
+    new Person(),
+    new Person()
   ];
 
+  //return number of people
   this.countPeople = function() {
     return this.people.length;
   };
 
+  //add a person
   this.addPerson = function(){
-    const newId = this.countPeople() + 1;
-    this.people.push(new Person(newId));
+    this.people.push(new Person());
   };
+
+  //remove a person
+  this.removePerson = function(id){
+    if(!id){
+      return false;
+    } else {
+      this.people = this.people.filter(e => e.id !== id);
+    }
+  };
+
+  //update a person
+  this.updatePerson = function(id, type, amount){
+    if(!id || !type){
+      return false;
+    } else {
+      this.people = this.people.map(e => {
+        if(e.id != id){
+          return e;
+        } else {
+          return {id, type, amount}
+        }
+      });
+    };
+  };
+
+  //read a person
+  this.readPerson = function(id){
+    if(!id){
+      return false;
+    } else {
+      return this.people.find(e => e.id === id) || false;
+    };
+  };
+
 };
 
 module.exports = {Bill, Person};
--- tests/bill.test.js
+++ tests/bill.test.js
@@ -1,22 +1,107 @@
 const {Bill, Person} = require('../src/bill');
 // import {Bill, Person} from '../src/bill';
 
+//mocks
+//http://untangled.io/how-to-mock-date-now-to-keep-date-based-unit-tests-predictable/
+function mockDateNow() {
+  // mock now = 1462361249717ms = 4th May 2016
+  return 1462361249717;
+}
+
+beforeEach(function () {
+  originalDateNow = Date.now;
+  Date.now = mockDateNow;
+});
+
+afterEach(function () {
+  Date.now = originalDateNow;
+});
+
+const fakeID = mockDateNow();
+const fakePerson = {id:fakeID, type:"split", amount:0}
+
 //default settings
 test('instantiate a Bill class', () => {
   const newBill = new Bill(100);
   expect( newBill.countPeople() ).toBe(2);
   expect( newBill.amount ).toBe(100);
   expect( newBill.service ).toBe(true);
+  expect( newBill.people ).toMatchObject([fakePerson, fakePerson]);
 }); 
 
-//add person
-test('add a person', () => {
-  const newBill = new Bill(100, false);
+//create a person
+test('instantiate a person', () => {
+  const newPerson = new Person();
+  expect(newPerson).toMatchObject(fakePerson);
+}); 
+
+//add a person
+test('create a person', () => {
+  const newBill = new Bill(100);
   newBill.addPerson();
   expect( newBill.countPeople() ).toBe(3);
-  expect( newBill.amount ).toBe(100);
-  expect( newBill.service ).toBe(false);
-  
-  const newPerson = new Person(2);
-  expect( JSON.stringify(newPerson) ).toMatch(JSON.stringify({id:2, type:"split"}));
+  expect( newBill.people[2] ).toMatchObject(fakePerson);
+}); 
+
+//remove a person
+test('remove a person', () => {
+  const newBill = new Bill(100);
+  const newId = fakeID + 1;
+  const newPerson = {id:newId, type:"split", amount:0};
+  newBill.people[0].id = newId;
+  newBill.removePerson(fakeID);
+  expect( newBill.countPeople() ).toBe(1);
+  expect( newBill.people[0] ).toMatchObject(newPerson);
+}); 
+
+test("don't remove a person", () => {
+  const newBill = new Bill(100);
+  newBill.removePerson(12);
+  expect( newBill.countPeople() ).toBe(2);
+  expect( newBill.people ).toMatchObject([fakePerson, fakePerson]);
+}); 
+
+//update a person
+test("update a person", () => {
+  const newBill = new Bill(100);
+  const newId = fakeID + 1;
+  const newPerson = {id:newId, type:"total", amount:12};
+  newBill.people[0].id = newId;
+  newBill.updatePerson(newPerson.id, newPerson.type, newPerson.amount);
+  expect( newBill.countPeople() ).toBe(2);
+  expect( newBill.people ).toMatchObject([newPerson, fakePerson]);
+}); 
+
+test("don't update a person", () => {
+  const newBill = new Bill(100);
+  const flag = newBill.updatePerson();
+  expect( newBill.countPeople() ).toBe(2);
+  expect( newBill.people ).toMatchObject([fakePerson, fakePerson]);
+  expect(flag).toBe(false);
+}); 
+
+test("don't update a person", () => {
+  const newBill = new Bill(100);
+  const flag = newBill.updatePerson(fakePerson.id);
+  expect( newBill.countPeople() ).toBe(2);
+  expect( newBill.people ).toMatchObject([fakePerson, fakePerson]);
+  expect(flag).toBe(false);
+}); 
+
+//read a person
+test("read a person", () => {
+  const newBill = new Bill(100);
+  const newId = fakeID + 1;
+  const newPerson = {id:newId, type:"split", amount:0};
+  newBill.people[0].id = newId;
+  const read = newBill.readPerson(newId);
+  expect( newBill.countPeople() ).toBe(2);
+  expect(read).toMatchObject(newPerson);
+}); 
+
+test("don't read a person", () => {
+  const newBill = new Bill(100);
+  const read = newBill.readPerson(12);
+  expect( newBill.countPeople() ).toBe(2);
+  expect(read).toBe(false);
 }); 
