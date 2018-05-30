91dae1111bab9713f1598fce42b4764308c7cb45
Nick M <nickmontagni@hotmail.com>
2018-05-30 13:01:37 +0100

    refactored controller tests with before each

--- tests/controller.test.js
+++ tests/controller.test.js
@@ -2,23 +2,43 @@ const { Bill, Person } = require('../src/bill');
 const { ViewBill } = require('../src/view');
 const { billController } = require('../src/controller');
 
-const newView = new ViewBill();
-const newModel = new Bill(100);
-const fakeId = 1462361249717;
-//same as mock ids for snapshots
-newModel.people[0].id = fakeId;
-newModel.people[1].id = fakeId + 1;
+// const newView = new ViewBill();
+// const newModel = new Bill(100);
+// const fakeId = 1462361249717;
+// //same as mock ids for snapshots
+// newModel.people[0].id = fakeId;
+// newModel.people[1].id = fakeId + 1;
 
-const dom = document.createElement("div");
-const testCon = new billController(newView, newModel, dom);
-testCon.createApp();
+// const dom = document.createElement("div");
+// const testCon = new billController(newView, newModel, dom);
+// testCon.createApp();
+
+//implementing before each testing structure
+let newView, newModel, fakeId, dom, testCon;
+const startingNumber = 2;
+
+beforeEach(()=>{
+  newView = new ViewBill();
+  newModel = new Bill(100);
+  fakeId = 1462361249717;
+  //same as mock ids for snapshots
+  newModel.people[0].id = fakeId;
+  newModel.people[1].id = fakeId + 1;
+
+  dom = document.createElement("div");
+  testCon = new billController(newView, newModel, dom);
+  testCon.createApp();
+});
 
 test('instantiate a controller class', () => {
+  expect( testCon.model.countPeople() ).toBe(startingNumber);
   expect(dom).toMatchSnapshot();
 });
 
 test('add a person', ()=>{
-  dom.querySelectorAll("button")[2].click();
+  expect( testCon.model.countPeople() ).toBe(startingNumber);
+  // dom.querySelectorAll("button")[2].click();
+  dom.querySelector("button.addPersonBtn").click()
   expect( testCon.model.countPeople() ).toBe(3);
   expect( dom.querySelectorAll("li").length ).toBe(3);
   const shares = [];
@@ -29,5 +49,9 @@ test('add a person', ()=>{
 });
 
 test('remove a person', ()=>{
-  console.log( testCon.model.countPeople() );
+  expect( testCon.model.countPeople() ).toBe(startingNumber);
+  dom.querySelector("button").click();
+  expect( testCon.model.countPeople() ).toBe(1);
+  expect( dom.querySelectorAll("li").length ).toBe(1);
+  expect( dom.querySelectorAll("span")[0].innerHTML ).toBe('Share: 100'); 
 });
